68a36c25cdfa4497321b84b939eb750c3d6e20a9
Mitchell <soggybag@gmail.com>
2019-09-29 22:00:32 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,261 @@
+// *************************************************
+// Game thing
+// *************************************************
+
+// -------------------------------------------------
+// Refereneces 
+
+const left_btn = document.getElementById('left-btn')
+const up_btn = document.getElementById('up-btn')
+const down_btn = document.getElementById('down-btn')
+const right_btn = document.getElementById('right-btn')
+const sprite = document.getElementById('sprite')
+const coords = document.getElementById('coords')
+const container = document.getElementById('container')
+const scoreCard = document.getElementById('score-card')
+  
+// --------------------------
+// Variables 
+
+const xyOffset = 64;
+
+let score = 0
+
+let x = xyOffset
+let y = xyOffset
+let targetX = x 
+let targetY = y
+const minX = xyOffset
+const maxX = xyOffset + (100 * 3)
+const minY = xyOffset
+const maxY = xyOffset + (100 * 3)
+let direction = 1
+
+
+function moveSprite() {
+  x -= (x - targetX) * 0.1
+  sprite.style.setProperty('--x', x)
+  y -= (y - targetY) * 0.1
+  sprite.style.setProperty('--y', y)
+}
+  
+function updateCoords() {
+  // coords.innerHTML = `X:${x.toFixed(1)} Y:${y.toFixed(1)}`
+}
+  
+function updateBackground() {
+  container.style.setProperty('--x', x / -1)
+  container.style.setProperty('--y', y / -1)
+}
+
+function moveLeft() {
+  if (targetX > minX) {
+    targetX -= 100
+    updateBackground()
+  }
+}
+
+function moveRight() {
+  if (targetX < maxX) {
+    targetX += 100
+    updateBackground()
+  }
+}
+
+function moveUp() {
+  if (targetY > minY) {
+    targetY -= 100
+    updateBackground()
+  }
+}
+
+function moveDown() {
+  if (targetY < maxY) {
+    targetY += 100
+    updateBackground()
+  }
+}
+  
+// Controls 
+left_btn.onclick = function(e) {
+  moveLeft()
+}
+  
+right_btn.onclick = function(e) {
+  moveRight()
+}
+  
+up_btn.onclick = function(e) {
+  moveUp()
+}
+  
+down_btn.onclick = function(e) {
+  moveDown()
+}
+
+// Listen for keys
+
+document.onkeydown = function(e) {
+  const { code } = e
+  switch(code) {
+    case 'ArrowLeft': 
+      moveLeft()
+      break
+    case 'ArrowRight':
+      moveRight()
+      break
+    case 'ArrowUp':
+      moveUp()
+      break
+    case 'ArrowDown':
+      moveDown()
+      break
+  }
+}
+  
+function random(range) {
+  return Math.floor(Math.random() * range)
+}
+  
+const bombs = []
+const types = ['bomb', 'apple', 'lemon', 'strawberry']
+
+function makeBomb() {
+  const el = document.createElement('div')
+  const type = types[random(types.length)]
+  el.classList.add(type)
+  container.appendChild(el)
+
+  const randomXY = random(4) * 100 + xyOffset
+  let x = 0
+  let y = 0
+  let dx = 0
+  let dy = 0
+  
+  switch(random(4)) {
+    case 0: // top
+      x = randomXY
+      y = -100
+      dx = 0
+      dy = 1
+      break
+
+    case 1: // right
+      x = 500
+      y = randomXY
+      dx = -1
+      dy = 0
+      break
+
+    case 2: // bottom
+      x = randomXY
+      y = 500
+      dx = 0
+      dy = -1
+      break
+
+    case 3: // left
+      x = -100
+      y = randomXY
+      dx = 1
+      dy = 0
+      break
+  }
+
+  el.style.left = `${x}px`
+  el.style.top = `${y}px`
+
+  // x = 0 + xyOffset
+  // y = 300 + xyOffset
+  // dx = 0
+  // dy = 0
+
+  dx = type === 'bomb' ? dx * 3 : dx * 1
+  dy = type === 'bomb' ? dy * 3 : dy * 1
+
+  const bomb = {
+    el,
+    x, y,
+    dx, dy, 
+    type
+  }
+
+  bombs.push(bomb)
+}
+
+function makeExplosion(x, y) {
+  const el = document.createElement('div')
+  container.appendChild(el)
+  el.classList.add('explosion')
+  el.style.left = `${x}px`
+  el.style.top = `${y}px`
+  setTimeout(() => {
+    el.parentNode.removeChild(el)
+  }, 1000)
+}
+
+function makeSparklyExplosion(x, y) {
+  const el = document.createElement('div')
+  container.appendChild(el)
+  el.classList.add('explosion-sparkly')
+  el.style.left = `${x}px`
+  el.style.top = `${y}px`
+  setTimeout(() => {
+    el.parentNode.removeChild(el)
+  }, 1000)
+} 
+  
+setInterval(function() {
+  makeBomb()
+}, 2000)
+
+makeBomb()
+
+function onFrame() {
+  moveSprite()
+  updateCoords()
+  bombs.forEach((bomb, i, arr) => {
+    const { el, dx, dy } = bomb
+    bomb.x += dx
+    bomb.y += dy
+    el.style.left = `${bomb.x}px`
+    el.style.top = `${bomb.y}px`
+
+    if (checkForCollision(bomb, {x, y})) {
+      if (bomb.type === 'bomb') {
+        // do game over stuff
+        score -= 5000
+        scoreCard.innerHTML = score
+        makeExplosion(bomb.x - 32, bomb.y - 32)
+      } else {
+        score += 1000
+        scoreCard.innerHTML = score
+        makeSparklyExplosion(bomb.x - 32, bomb.y - 32)
+      }
+      el.parentNode.removeChild(el)
+      arr.splice(i, 1)
+      return
+    }
+
+    if (bomb.x < -100 || bomb.x > 500 || bomb.y < -100 || bomb.y > 500) {
+      el.parentNode.removeChild(el)
+      arr.splice(i, 1)
+    }
+  })
+
+  requestAnimationFrame(onFrame)
+}
+
+requestAnimationFrame(onFrame)
+
+function checkForCollision(obja, objb) {
+  const dx = Math.abs(obja.x - objb.x)
+  const dy = Math.abs(obja.y - objb.y)
+  const offset = xyOffset / 1
+
+  if (dx < offset && dy < offset) {
+    return true
+  }
+
+  return false
+}
---
+++ readme.md
@@ -0,0 +1,36 @@
+# Demo Game 
+
+This is demo app to give an idea of the kinds of code and programming involved with programming web applications. 
+
+## Todo - 
+
+1. Some tasks to explore programming on the web.
+  - Include comments with notes on possible values along side code
+  - Building Web Applications 
+    - Need a branch to demo the completed game 
+    - Master branch should leave out some things that become and activity
+      - Links to css files and js files
+        - These files might be broken into separate files 
+  - HTML 
+    - Add some images of other characters
+    - Add CSS styles 
+    - Add JS file 
+  - CSS 
+    - Can be better organized for demo
+    - Add opportunities for students to make improvements to the appearance 
+      - #container overflow hidden 
+      - #contanier round corner
+      - .button round corner changes colors
+      - .button:hover change colors 
+    - Change game images used as game elements 
+      - --player-image
+      - --bg-image
+      - --bomb-image
+      - --fruit-*
+  - JS 
+    - Add link to main.js
+    - Change somethings in the JS to see what happens
+      - Edit variables 
+        - change the speed and frequency of elements 
+          - speed of bombs and fruit
+          - frequency of fruit vs bombs
